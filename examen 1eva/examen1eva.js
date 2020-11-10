28f79c40d2d1d02785392e285d8fdb88e5a9e107
lucia8716 <crespopopo@gmail.com>
2020-11-10 10:19:42 +0100

    v1

--- examen 1eva/examen1eva.js
+++ examen 1eva/examen1eva.js
@@ -6,28 +6,54 @@ var pro ={refpro:1,nombrecomun:'primperan',composicion:'Metoclopramida',laborato
 var pro2 ={refpro:2,nombrecomun:'ibuprofeno',composicion:'ibuprofeno',laboratorio:'Piffer',preciocompra:5.74,precioventa:6.87,unidades:3};
 var pro3 ={refpro:3,nombrecomun:'fosrenol',composicion:'lantano',laboratorio:'shire',preciocompra:6.74,precioventa:9.87,unidades:45};
 var pro4 ={refpro:4,nombrecomun:'finasterina',composicion:'finasterida',laboratorio:'Piffer',preciocompra:13.74,precioventa:23.87,unidades:14};
-
-
 var productos =[pro,pro2,pro3,pro4];
+var referencia=[];
+var contfuncioncompras=0;
 
-function compras (){
-    
+
+function iracompras(){
+
     document.getElementById("f1").style.visibility = "hidden";
     document.getElementById("f2").style.visibility = "visible";
-
-var pedro={refpro:5,nombrecomun:'finasterina',composicion:'finasterida',laboratorio:'Piffer',preciocompra:13.74,precioventa:23.87,unidades:14};
-
-
-
-productos.push(pedro);
-
-
-alert("añadido");
-
-
-
-
 }
+
+
+
+function compras (){
+var x1=document.getElementById("i1").value;
+if(x1>0&&x1<999999999999){
+if (contfuncioncompras==0) {
+    referencia.push(x1);
+    contfuncioncompras++;
+    document.getElementById("i1").value="";
+       
+} else {
+var cont=0;
+var comprobar=false;
+    for ( i = 0; i < referencia.length; i++) {
+        if (x1==referencia[i]) {
+            i=referencia.length;
+        alert("Nº Referencia se encuentra en bbdd");
+        } 
+        }
+
+       
+
+
+
+
+
+    }
+
+}else{
+    document.getElementById("i1").value="";
+    document.getElementById("i1").focus();
+    alert("incorrecto");
+}
+}
+
+
+
 
 function visualizar (){
 
