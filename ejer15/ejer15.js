512493499bdd19595da4d0af153bd8bfd84c232d
lucia8716 <crespopopo@gmail.com>
2020-11-19 14:01:05 +0100

    v1

---
+++ ejer15/ejer15.js
@@ -0,0 +1,35 @@
+function abrir() {
+
+
+    document.getElementById("f1").style.visibility = "hidden";
+    document.getElementById("f2").style.visibility = "visible";
+    document.getElementById("f3").style.visibility = "hidden";
+    document.getElementById("f4").style.visibility = "hidden";
+}
+
+function abrir1() {
+
+
+    document.getElementById("f1").style.visibility = "hidden";
+    document.getElementById("f2").style.visibility = "hidden";
+    document.getElementById("f3").style.visibility = "visible";
+    document.getElementById("f4").style.visibility = "hidden";
+}
+
+function abrir2() {
+
+
+    document.getElementById("f1").style.visibility = "hidden";
+    document.getElementById("f2").style.visibility = "hidden";
+    document.getElementById("f3").style.visibility = "hidden";
+    document.getElementById("f4").style.visibility = "visible";
+}
+
+function volver() {
+
+
+    document.getElementById("f1").style.visibility = "visible";
+    document.getElementById("f2").style.visibility = "hidden";
+    document.getElementById("f3").style.visibility = "hidden";
+    document.getElementById("f4").style.visibility = "hidden";
+}
