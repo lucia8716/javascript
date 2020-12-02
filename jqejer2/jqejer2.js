e12641f09cb0d5eaf23f00921114cf02142408fa
lucia8716 <crespopopo@gmail.com>
2020-12-02 10:56:35 +0100

    v1

--- jqejer2/jqejer2.js
+++ jqejer2/jqejer2.js
@@ -1,4 +1,19 @@
 //https://api.jquery.com/toggleclass/
-$( "img" ).click(function() {
+$( "#i1" ).click(function() {
   $( this ).toggleClass( "p1" );
+  $('#i3').toggle('slow');
+  $('#i2').toggle('slow');
+});
+
+
+$( "#i2" ).click(function() {
+  $( this ).toggleClass( "p1" );
+  $('#i1').toggle('slow');
+  $('#i3').toggle('slow');
+});
+
+$( "#i3" ).click(function() {
+  $( this ).toggleClass( "p1" );
+  $('#i1').toggle('slow');
+  $('#i2').toggle('slow');
 });
